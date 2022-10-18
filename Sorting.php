655d6fe61091b226e5c495ccbd7340e6043613ca
Shahidul-PHP <Shahidul.shovon14@gmail.com>
2022-10-18 20:04:07 +0600

    all done

--- Sorting.php
+++ Sorting.php
@@ -3,11 +3,15 @@
 echo '<title>Array Sorting Practice PHP</title>';
 
 
+$cars = array("Amra", "Cat", "Doll","Ball", "Fail", "Jamil","Kamil","Flower","Hasan","Garments");
+rsort($cars);
 
-
-
-
-
+$length = count($cars);
+for ($x = 0; $x < $length; $x++) {
+    echo $x;
+    echo $cars[$x];
+    echo "<br>";
+}
 
 
 

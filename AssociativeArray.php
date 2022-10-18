988b74d0654689397520a75777de6de87e44ab2b
Shahidul-PHP <Shahidul.shovon14@gmail.com>
2022-10-18 10:08:08 +0600

    associate array done

--- AssociativeArray.php
+++ AssociativeArray.php
@@ -1,11 +1,17 @@
 <?php 
 
-echo '<title>Associative Array</title>'
+echo '<title>Associative Array</title>';
 
 
+$mix = ['7'=>'apple', 'orange', 'drinks'=>'cha','cofee', 'hotfood'=>'Pizza', 'Burger','sweet','Chomchom','Rasgulla', 'Shahidul'];
 
 
+echo '<pre>';
+    print_r($mix);
+echo '</pre>';
 
+
+//echo $mix[3];
 
 
 
